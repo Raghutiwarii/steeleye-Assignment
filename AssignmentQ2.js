0d1c5e6c9808b90c59f688bc96982a5606467e6b
Raghunath Tiwari <75925472+Raghutiwarii@users.noreply.github.com>
2023-08-06 20:16:16 +0530

    Create AssignmentQ2.js

---
+++ AssignmentQ2.js
@@ -0,0 +1,46 @@
+/**
+ * Strips the prefix from the keys of the given key-value pairs
+ * @param {string} htmlContent - HTML content which needs to be highlighted 
+ * @param {string} plainText - This plain text is extracted from htmlContent
+ * @param {array} plainTextPositions - Array of Objects with start and end positions of words in plainText (Not the positions in HTML)
+ * @returns {string} Using the positions in plainText, find the appropriate positions in htmlContent, highlight the content and return it
+ */
+function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
+  const positionsMapping = plainTextPositions.map(({ start, end }) => ({
+    start: htmlContent.indexOf(plainText.substring(start, end)),
+    end: htmlContent.indexOf(plainText.substring(start, end)) + (end - start)
+  }));
+  positionsMapping.sort((a, b) => a.start - b.start);
+  let highlightedContent = htmlContent;
+  for (let i = positionsMapping.length - 1; i >= 0; i--) {
+    const { start, end } = positionsMapping[i];
+    highlightedContent = highlightedContent.substring(0, start) +
+      `<mark>${highlightedContent.substring(start, end)}</mark>` +
+      highlightedContent.substring(end);
+  }
+
+  return highlightedContent;
+}
+
+
+describe('highlightHTMLContent', () => {
+it('should highlight the specified positions in the HTML content', () => {
+  const htmlContent = '<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects<br><br>Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility <mark>Equity</mark> scale solar…<br><br>Read the full article <a href="https://content.seleritycorp.com/hosted/assets/www/UKMW47_hYz_RGzPSpHm44Hi1L49HdNBhs1OkKKW2OPI">here</a><br><br>-------------------------------------<br><br>You received this because you are subscribed to news related to <a href="https://iris.steeleye.co/market/instruments?search=ES0113900J37">ES0113900J37</a>, and this story was marked as 82% relevant.<br><br>Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | <mark>Privacy</mark> Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. <br><br>To unsubscribe change your email preferences, please click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>.<br><br>-------------------------------------<br><br><img src="https://context.seleritycorp.com/selerity/assets/sc_icons/pressRelease.png" alt="Rick Astley" style="width:100px;height:100px;"></span></p>'
+  const plainText =  'Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar… Read the full article here ------------------------------------- You received this because you are subscribed to news related to ES0113900J37 , and this story was marked as 82% relevant. Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. To unsubscribe change your email preferences, please click here . -------------------------------------'
+  const plainTextPositions = [
+  {
+      start: 241,
+      end: 247,
+  },
+  {
+      start: 518,
+      end: 525,
+  },
+];
+
+  const expectedHighlightedContent = '<p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects<br><br>Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility <mark>Equity</mark> scale solar…<br><br>Read the full article <a href="https://content.seleritycorp.com/hosted/assets/www/UKMW47_hYz_RGzPSpHm44Hi1L49HdNBhs1OkKKW2OPI">here</a><br><br>-------------------------------------<br><br>You received this because you are subscribed to news related to <a href="https://iris.steeleye.co/market/instruments?search=ES0113900J37">ES0113900J37</a>, and this story was marked as 82% relevant.<br><br>Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | <mark>Privacy</mark> Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. <br><br>To unsubscribe change your email preferences, please click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>.<br><br>-------------------------------------<br><br><img src="https://context.seleritycorp.com/selerity/assets/sc_icons/pressRelease.png" alt="Rick Astley" style="width:100px;height:100px;"></span></p>'
+  const highlightedContent = highlightHTMLContent(htmlContent, plainText, plainTextPositions);
+
+  expect(highlightedContent).toBe(expectedHighlightedContent);
+});
+});
